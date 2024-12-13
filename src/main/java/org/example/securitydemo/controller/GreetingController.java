39709f79556ff8b87df8cb8fd0d12af3b2b9b136
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-13 14:03:19 +0300

    add minor updates and Greeting controller

---
+++ src/main/java/org/example/securitydemo/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package org.example.securitydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class GreetingController {
+
+  @GetMapping
+  public String greet() {
+    return "Hello!";
+  }
+}
