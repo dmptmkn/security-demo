1ef7bae0a6fedf89befcb594c6035a35775e503d
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-16 13:43:29 +0300

    add custom UserDetails and UserDetailsService implementations

--- src/main/java/org/example/securitydemo/controller/GreetingController.java
+++ src/main/java/org/example/securitydemo/controller/GreetingController.java
@@ -2,6 +2,7 @@ package org.example.securitydemo.controller;
 
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/org/example/securitydemo/service/InMemoryUserDetailsService.java
@@ -0,0 +1,2 @@
+package org.example.securitydemo.service;public class InMemoryUserDetailsService {
+}
