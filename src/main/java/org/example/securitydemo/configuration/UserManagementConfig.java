7ebf81ec9be3c5420c633c89da17a0fcaf46cf8c
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-16 13:46:37 +0300

    add minor updates

--- src/main/java/org/example/securitydemo/configuration/UserManagementConfig.java
+++ src/main/java/org/example/securitydemo/configuration/UserManagementConfig.java
@@ -1,8 +1,10 @@
 package org.example.securitydemo.configuration;
 
+import java.util.List;
+import org.example.securitydemo.model.User;
+import org.example.securitydemo.service.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,25 +14,31 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class UserManagementConfig {
 
+//  @Bean
+//  public UserDetailsService userDetailsService() {
+//    InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+//
+//    UserDetails userDetails = User.builder()
+//        .username("John")
+//        .password("1234")
+//        .authorities("write")
+//        .build();
+//    userDetailsService.createUser(userDetails);
+//
+//    userDetails = User.builder()
+//        .username("Ivan")
+//        .password("1234")
+//        .authorities("read")
+//        .build();
+//    userDetailsService.createUser(userDetails);
+//
+//    return userDetailsService;
+//  }
+
   @Bean
   public UserDetailsService userDetailsService() {
-    InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-
-    UserDetails userDetails = User.builder()
-        .username("John")
-        .password("1234")
-        .authorities("write")
-        .build();
-    userDetailsService.createUser(userDetails);
-
-    userDetails = User.builder()
-        .username("Ivan")
-        .password("1234")
-        .authorities("read")
-        .build();
-    userDetailsService.createUser(userDetails);
-
-    return userDetailsService;
+    User user = new User("Ivan", "1234", "read");
+    return new InMemoryUserDetailsService(List.of(user));
   }
 
   @Bean
--- src/main/java/org/example/securitydemo/service/InMemoryUserDetailsService.java
+++ src/main/java/org/example/securitydemo/service/InMemoryUserDetailsService.java
@@ -1,2 +1,23 @@
-package org.example.securitydemo.service;public class InMemoryUserDetailsService {
+package org.example.securitydemo.service;
+
+import java.util.List;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class InMemoryUserDetailsService implements UserDetailsService {
+
+  private final List<UserDetails> users;
+
+  public InMemoryUserDetailsService(List<UserDetails> users) {
+    this.users = users;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return users.stream()
+        .filter(userDetails -> userDetails.getUsername().equals(username))
+        .findFirst()
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
 }
