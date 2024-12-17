213e0cf3ea8513264314e64baf8c929bee7cb541
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-17 17:40:53 +0300

    change inheritance for logging filter

--- src/main/java/org/example/securitydemo/filter/AuthenticationLoggingFilter.java
+++ src/main/java/org/example/securitydemo/filter/AuthenticationLoggingFilter.java
@@ -6,21 +6,21 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.logging.Logger;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class AuthenticationLoggingFilter implements Filter {
+public class AuthenticationLoggingFilter extends OncePerRequestFilter {
 
   private final Logger logger = Logger.getLogger(AuthenticationLoggingFilter.class.getName());
 
   @Override
-  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
-      FilterChain filterChain) throws IOException, ServletException {
-    HttpServletRequest request = (HttpServletRequest) servletRequest;
-
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
     String requestId = request.getHeader("Request-Id");
     logger.info("Successfully authenticated request with id %s".formatted(requestId));
 
-    filterChain.doFilter(servletRequest, servletResponse);
+    filterChain.doFilter(request, response);
   }
 }
