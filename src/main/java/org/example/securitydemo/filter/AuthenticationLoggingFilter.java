c396bbd0bab127b26996bd6bae013064e4a0c687
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-18 23:13:06 +0300

    change imports for logging filter

--- src/main/java/org/example/securitydemo/filter/AuthenticationLoggingFilter.java
+++ src/main/java/org/example/securitydemo/filter/AuthenticationLoggingFilter.java
@@ -1,10 +1,7 @@
 package org.example.securitydemo.filter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
